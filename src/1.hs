ca9daa0fa2a3d53e6e93372071279249e1a367a1
Archim Jhunjhunwala <archim.jhunjhunwala@gmail.com>
2024-12-01 14:15:16 -0500

    day 1

--- src/1.hs
+++ src/1.hs
@@ -1,5 +1,29 @@
-main :: IO()
-main = putStrLn (greet "World")
+import Data.List (sort)
+import GHC.Base (eqInt)
+import System.IO
 
-greet :: String -> String
-greet who = "Hello" ++ who
+main :: IO ()
+main = do
+  handle <- openFile "inputs/1-actual.txt" ReadMode
+  contents <- hGetContents handle
+
+  putStrLn "Part I:"
+  print $ sum $ zipWith diff (column 0 (lines contents)) (column 1 (lines contents))
+
+  putStrLn "Part II:"
+  -- slow O(n^2) way
+  print $ sum $ map (entrySimScore (column 1 (lines contents))) (column 0 (lines contents))
+
+  hClose handle
+
+entrySimScore :: [Int] -> Int -> Int
+entrySimScore list a = a * length (filter (eqInt a) list)
+
+diff :: Int -> Int -> Int
+diff a b = abs $ a - b
+
+column :: Int -> [String] -> [Int]
+column col lines_ = sort (map (parseLine col) lines_)
+
+parseLine :: Int -> String -> Int
+parseLine col line = read (words line !! col)
