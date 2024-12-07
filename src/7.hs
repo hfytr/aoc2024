ccfaa5bd3f23a0ef3b8ccc1a60be612a345cb87d
Archim Jhunjhunwala <archim.jhunjhunwala@gmail.com>
2024-12-07 13:28:23 -0500

    day07

---
+++ src/7.hs
@@ -0,0 +1,21 @@
+main :: IO ()
+main = do
+  contents <- readFile "inputs/7-actual.txt"
+  let input = parseInput $ lines contents
+  putStrLn "Part I:"
+  print $ solve [(+), (*)] input
+  putStrLn "Part II:"
+  print $ solve [(+), (*), concatNums] input
+
+solve :: [Int -> Int -> Int] -> [(Int, [Int])] -> Int
+solve ops list = sum $ map fst $ filter (\(t, l) -> checkLine ops (head l) t (tail l)) list
+
+checkLine :: [Int -> Int -> Int] -> Int -> Int -> [Int] -> Bool
+checkLine _ left target [] = left == target
+checkLine ops left target (ni : ntail) = any (\op -> checkLine ops (left `op` ni) target ntail) ops
+
+concatNums :: Int -> Int -> Int
+concatNums a b = a * 10 ^ length (show b) + b
+
+parseInput :: [String] -> [(Int, [Int])]
+parseInput = map (\s -> ((read . init . head . words) s, map read ((tail . words) s)))
